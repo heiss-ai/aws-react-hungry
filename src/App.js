7d31741be10f5d878b918a45e64696f0b4ef25ce
Brandon Heiss <bheiss50@gmail.com>
2021-02-04 08:13:23 -0600

    ??

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <p>"Doge to the MOON" - not not Brandon Heiss</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="adver">..hiring graphic designers</p>
+        <p className="adver">i'm sick of mothafuckas tryna tell me how to live</p>
       </header>
     </div>
   );
