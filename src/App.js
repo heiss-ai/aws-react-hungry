519b81d5b88ef051eef65e596ec8c30a2100dbba
Brandon Heiss <bheiss50@gmail.com>
2021-02-16 08:38:24 -0600

    revert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './photo.png';
+import logo from './doge.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <p>About to pop off</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="adver">i'm sick of mothafuckas tryna tell me how to live</p>
+        <p className="adver">"doge to the moon!" - not Brandon Heiss</p>
       </header>
     </div>
   );
